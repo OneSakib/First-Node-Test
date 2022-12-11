76407008193d7e54f9451ec17be80c7e34734776
Sakib Malik <maliksakib347@gmail.com>
2022-12-11 18:52:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let x = 10;
+let y = 20;
+z = function () {
+    return x + y;
+}
+
+module.exports = { x, y, z }
---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http')
+var colors = require('colors');
+
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  });
+   
+  // outputs red text
+  console.log("this is an error".error);
+   
+  // outputs yellow text
+  console.log("this is a warning".warn);
